ad98701e5e763ee642bb8d572c685a5d6662b69d
Unknown Decoder <155003129+UnknownDecoder@users.noreply.github.com>
2025-07-14 21:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const chatForm = document.getElementById('chat-form');
-const userInput = document.getElementById('user-input');
+const userInput = document.querySelector('.user-input');
 const messagesDiv = document.getElementById('messages');
+const sendBtn = document.querySelector('.sendBtn')
 var wrapped = document.querySelector('.wrapped');
 var cont = document.querySelector('.container');
 
+userInput.addEventListener('input', () => {
+    if (userInput.value.trim() === '') {
+        sendBtn.disabled = true;
+    } else {
+        sendBtn.disabled = false;
+    }
+})
 
-chatForm.addEventListener('submit', function (e) {
-  e.preventDefault();
+function sendMessage() {
+    const message = userInput.value.trim();
+    if (message !== '') {
+        displayMessage(message, "user");
+        userInput.value = '';
+        sendBtn.disabled = true;
+    }
+    setTimeout(() => getResponse(message), 2000);
 
-  const message = userInput.value.trim();
-  if (message !== '') {
-    displayMessage(message);
-    userInput.value = '';
-  }
+}
+
+
+sendBtn.addEventListener('click', sendMessage);
+userInput.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        sendMessage()
+    }
 });
 
-function displayMessage(text) {
+
+function getResponse(msg) {
+    fetch(`https://decoder-brain.onrender.com/chat?msg=${encodeURIComponent(msg)}`)
+        .then(res => res.json())
+        .then(data => {
+            displayMessage(data.reply, data.name)
+        }).catch((error) => console.log(error))
+}
+
+function displayMessage(text, sender) {
   const msgDiv = document.createElement('div');
-  msgDiv.classList.add('message', 'user-message'); // Add second class
+  if (sender==="user") {
+      msgDiv.classList.add('message', 'user-message');
+  } else {
+      msgDiv.classList.add('message')
+  }
   msgDiv.textContent = text;
   messagesDiv.appendChild(msgDiv);
   messagesDiv.scrollTop = messagesDiv.scrollHeight;
@@ -31,7 +61,7 @@ function loading(){
 
 
 // if u wanna decrease or increase the loading time
-setTimeout(loading, 7000)
+setTimeout(loading, 3000)
 
 
 let label = document.querySelector('.label');
